71c5c7e9595010f8ebf7dd801fa637f084a7cde8
Jacob Sayono <jrsayono@gmail.com>
2023-01-17 19:48:32 -0800

    setf

---
+++ notes/setf.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int I = 100;
+
+    /* how to quit early
+    if (I == 100)
+    {
+        // leave right away
+        return 1; // exit error code associated with 100
+    } */
+
+    const double D = 10.0000;
+    
+    // setf: set flag
+    cout.setf(ios::showpoint);  // show decimal
+    cout.setf(ios::fixed);      // fixed notation (not scientific notation)
+    cout.precision(2);          // 2 digits after decimal
+    cout << I << endl;  // 10
+    cout << D << endl;  // 10.00
+    
+    return 0;
+}
