dd1144e18039ceeb6faf800801dd1965dd68c72f
Jacob Sayono <jrsayono@gmail.com>
2023-01-23 22:50:59 -0800

    prime

--- practices/prime.cpp
+++ practices/prime.cpp
@@ -9,15 +9,20 @@
 using namespace std;
 
 int main() {
+    // n: user input
+    // x: iteration variable to test for divisibility of a number
+    // c: the actual prime number
+
     int n, x;
     cout << "enter your number: ";
     cin >> n;
+
     int c = 3;
-    while (c < n) {
+    while (c <= n) {  // check for every number from 3 to given input n
         bool isPrime = true;
-        x = 2;
-        while (x < c) {  // c must be divisible by any number from 2 to c-1
-            if (c % x == 0) {
+        x = 2;  // from 2
+        while (x < c) {  // to c-1
+            if (c % x == 0) {  // c must be divisible by any number from 2 to c-1
                 isPrime = false;
             }
             x++;
@@ -26,6 +31,20 @@ int main() {
             cout << c << " ";
         }
         c++;
+    }
+    cout << endl;
+
+    // for loop version
+    for (int c = 3; c <= n; c++) {  // from 3 to n
+        bool isPrime = true;
+        for (int x = 2; x < c; x++) {  // from 2 to (c-1)
+            if (c % x == 0) {  // if c is divisible by tester x
+                isPrime = false;  // then c is not a prime number
+            }  // just need to hit divisibility property once to turn off boolean and skip the print
+        }
+        if (isPrime) {  // if false, then skip to x++
+            cout << c << " ";
+        }
     }
     cout << endl;
 
