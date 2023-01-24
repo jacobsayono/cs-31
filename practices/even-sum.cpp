4fafa5eed83f856a64e60c70cfa1607d5a4212ee
Jacob Sayono <jrsayono@gmail.com>
2023-01-23 22:58:03 -0800

    even-sum

--- practices/even-sum.cpp
+++ practices/even-sum.cpp
@@ -10,3 +10,20 @@
 // number4: 6
 // result: 8
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    // for loop version
+    int x, sum = 0;
+    cout << "enter 4 numbers: " << endl;
+    for (int i = 0; i < 4; i++) {
+        cout << "number" << i+1 << ": ";
+        cin >> x;
+        
+        if (x % 2 == 0) {
+            sum += x;
+        }
+    }
+    cout << "result: " << sum << endl;
+}
