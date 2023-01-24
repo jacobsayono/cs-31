7b3c35256b104e3c0f6bddb12a82cffd6c742629
Jacob Sayono <jrsayono@gmail.com>
2023-01-23 22:32:09 -0800

    prime

--- practices/prime.cpp
+++ practices/prime.cpp
@@ -2,29 +2,30 @@
 // it has a number of bugs
 // fix them and replace the inner while loop with a for loop
 
-// n is a prime number if n is not divisible by any number other than 1 and n
-// in other words, n is a prime number if n is divisible by any number from 2 to n-1
+// c is a prime number if it is not divisible by any number other than 1 and n
+// in other words, c is a prime number if it is divisible by any number from 2 to n-1
 
 #include <iostream>
 using namespace std;
 
 int main() {
     int n, x;
-    cin >> x;
-    int candidate = 3;
-    while (candidate < n) {
+    cout << "enter your number: ";
+    cin >> n;
+    int c = 3;
+    while (c < n) {
         bool isPrime = true;
         x = 2;
-        while (x < n) {  // replace with a for loop
-            if (candidate % x == 0) {
+        while (x < c) {  // c must be divisible by any number from 2 to c-1
+            if (c % x == 0) {
                 isPrime = false;
             }
-            x = x + 1;
-            if (isPrime) {
-                cout << n << " ";
-            }
+            x++;
         }
-        candidate++;
+        if (isPrime) {
+            cout << c << " ";
+        }
+        c++;
     }
     cout << endl;
 
