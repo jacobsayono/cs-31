d7fd42be4c84d5ea251025a64b0fc62e16d60909
Jacob Sayono <jrsayono@gmail.com>
2023-02-23 21:41:48 -0800

    proj-5

---
+++ project-5/MealPlan.cpp
@@ -0,0 +1,25 @@
+#include "MealPlan.h"
+
+MealPlan::MealPlan() {
+
+}
+
+void MealPlan::pickPlan(plan p) {
+
+}
+
+plan MealPlan::getPlan() {
+
+}
+
+double MealPlan::cost() {
+
+}
+
+int MealPlan::mealsEachWeek() {
+
+}
+
+bool MealPlan::unusedMealsCarryOver() {
+    
+}
